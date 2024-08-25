e307f104e0b6b31c177c9ab10a43f638ac005644
Marc-Antoine Desjardins <marcantoinedesjardins@gmail.com>
2024-08-25 00:27:22 -0400

    Remove old test launchers

--- test/test_camera.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set AIWARMACHINE_PYTHON_DIR=%~dp0..\python
-set PYTHONPATH=%PYTHONPATH%;%AIWARMACHINE_PYTHON_DIR%
-
-call python %AIWARMACHINE_PYTHON_DIR%\AiWarmachine\test\test_camera.py
-
-timeout /t 3
