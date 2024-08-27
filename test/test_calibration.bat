10fab9f21fc5ad0e8f4f8c9f46bedd40471d0d2f
Marc-Antoine Desjardins <marcantoinedesjardins@gmail.com>
2024-08-27 00:42:43 -0400

    Remove old Calibration launcher

--- test/test_calibration.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set AIWARMACHINE_PYTHON_DIR=%~dp0..\python
-set PYTHONPATH=%PYTHONPATH%;%AIWARMACHINE_PYTHON_DIR%;
-
-call python %AIWARMACHINE_PYTHON_DIR%\AiWarmachine\launch\launch_aiwarmachine.py
-
-timeout /t 10
